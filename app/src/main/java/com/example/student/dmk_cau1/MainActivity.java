8bfa75c73463454b1717e3b6e12364f961e52938
dangminhkha <skymost21@gmail.com>
2019-09-09 16:37:45 +0700

    up code cau 2

---
+++ app/src/main/java/com/example/student/dmk_cau1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.student.dmk_cau1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edt_TaiKhoan, edt_MatKhau;
+    CheckBox check_Luu;
+    Button btn_DangNhap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt_TaiKhoan = (EditText) findViewById(R.id.edt_TaiKhoan);
+        edt_MatKhau = (EditText) findViewById(R.id.edt_MatKhau);
+
+        check_Luu = (CheckBox) findViewById(R.id.check_Luu);
+        btn_DangNhap = (Button) findViewById(R.id.btn_DangNhap);
+        btn_DangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (check_Luu.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đã đăng nhập hệ thống, Thông tin của bạn đã được lưu", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đã đăng nhập hệ thống, Thông tin của bạn không được lưu", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
